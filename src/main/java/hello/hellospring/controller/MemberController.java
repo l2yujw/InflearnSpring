3e6bd5fe81b1835a52f1b81f3217c67e0fde2445
ryu <jeungwon28@gmail.com>
2023-12-18 20:46:47 +0900

    ADD: 웹 기능 - 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,23 @@ public class MemberController {
     @Autowired //memberService를 스프링이 스프링컨테이너에 있는 memberService와 연결을 시켜줌
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+
+    //MemberForm안에 있는 name에 post를 통해 제공된 값이 들어온다.
+    //스프링이 setName을 통해 값을 넣어주게 됌
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }

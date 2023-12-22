48dcc7b905d032a9a3a2d52a82a8997163e26693
ryu <jeungwon28@gmail.com>
2023-12-22 20:44:57 +0900

    ADD: 다양한 의존관계 주입 방법들

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,14 +13,62 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    /*
+    //필드 주입 (사용하지 말자)
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
+
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+    */
+
+
+    /*
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    //수정자 주입
+    @Autowired
+    public
+    void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }*/
+
+    //    @Autowired //생성자가 하나 일 때는 생략 가능
+
+
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
+//        System.out.println("memberRepository = " + memberRepository + ", discountPolicy = " + discountPolicy);
     }
+
+    /*
+    //일반 메서드 주입
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

543a6e6139156857d49a856e14a6b9a68596dbbe
ryu <jeungwon28@gmail.com>
2023-12-22 19:34:51 +0900

    ADD: Component 필터링

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        //member 경로만 스캔 대상이 됌
-        basePackages = "hello.core.member",
+        /* 지정하지 않으면 hello.core 부터 탐색을 함
+            //member 경로만 스캔 대상이 됌 (탐색할 패키지의 시작 위치를 지정)
+            basePackages = "hello.core.member",
+            basePackageClasses = AutoAppConfig.class,
+        */
         //Component를 찾을 때 뺄 것들을 정함
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
---
+++ src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,12 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+/**
+ *  어노테이션 학습 필요
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+}

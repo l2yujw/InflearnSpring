2c712b343588175cca546ae5d33ca850a971fd6e
ryu <jeungwon28@gmail.com>
2023-12-24 17:11:48 +0900

    ADD: lombok 미소짓게 만들어줌

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,13 +7,15 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //final 붙은 변수로 생성자를 만들어 줌
 public class OrderServiceImpl implements OrderService {
 
-    /*
+/*
     //필드 주입 (사용하지 말자)
     @Autowired private MemberRepository memberRepository;
     @Autowired private DiscountPolicy discountPolicy;
@@ -25,10 +27,9 @@ public class OrderServiceImpl implements OrderService {
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
     }
-    */
+*/
 
-
-    /*
+/*
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
@@ -44,10 +45,13 @@ public class OrderServiceImpl implements OrderService {
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         System.out.println("discountPolicy = " + discountPolicy);
         this.discountPolicy = discountPolicy;
-    }*/
+    }
+*/
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+/*
 
 //    @Autowired //생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
@@ -55,8 +59,9 @@ public class OrderServiceImpl implements OrderService {
         this.discountPolicy = discountPolicy;
 //        System.out.println("memberRepository = " + memberRepository + ", discountPolicy = " + discountPolicy);
     }
+*/
 
-    /*
+/*
     //일반 메서드 주입
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
@@ -66,7 +71,7 @@ public class OrderServiceImpl implements OrderService {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-    */
+*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

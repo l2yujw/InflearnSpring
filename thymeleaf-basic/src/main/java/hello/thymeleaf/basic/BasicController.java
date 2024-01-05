1584113a2f903f17f9214512942593808ac01855
ryu <jeungwon28@gmail.com>
2024-01-05 16:52:07 +0900

    ADD: 블록

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -124,6 +124,12 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
     @Data
     static class User {
         private String username;

4252e711e1c27d5ff4e9052026b508cd4f1fc274
ryu <jeungwon28@gmail.com>
2024-03-06 12:59:23 +0900

    Initialize

---
+++ SpringAdvancedJPA2/jpashop2/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(UpdateItemDto itemDto) {
+        Item findItem = itemRepository.findOne(itemDto.getItemId());
+        findItem.change(itemDto.getName(), itemDto.getPrice(), itemDto.getStockQuantity());
+    }
+
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}

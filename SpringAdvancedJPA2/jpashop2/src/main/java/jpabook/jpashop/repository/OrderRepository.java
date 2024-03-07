b84ae9a9d59b76a064eb3d01a49ea1ccb87355d0
ryu <jeungwon28@gmail.com>
2024-03-08 00:36:46 +0900

    ADD: 주문 조회V2:엔티티를 DTO로 변환

--- SpringAdvancedJPA2/jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ SpringAdvancedJPA2/jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -129,11 +129,5 @@ public class OrderRepository {
                 .getResultList();
     }
 
-    public List<OrderSimpleQueryDto> findOrderDtos() {
-        return em.createQuery("select new jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) from Order o" +
-                        " join o.member m" +
-                        " join o.delivery d", OrderSimpleQueryDto.class)
-                .getResultList();
-    }
 }
 

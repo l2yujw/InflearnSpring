0643c59067ca23302234d6e5fc8e334d9d4e7fe7
ryu <jeungwon28@gmail.com>
2024-03-09 23:27:20 +0900

    ADD: 파라미터 바인딩

---
+++ SpringDataJPA/data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
@@ -0,0 +1,19 @@
+package study.datajpa.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MemberDto {
+
+    private Long id;
+    private String username;
+    private String teamName;
+
+    public MemberDto(Long id, String username, String teamName) {
+        this.id = id;
+        this.username = username;
+        this.teamName = teamName;
+    }
+}

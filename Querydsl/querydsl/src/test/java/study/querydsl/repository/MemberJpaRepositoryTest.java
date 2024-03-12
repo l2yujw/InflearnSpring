e8dc4d20f4dfdb22d7ad250956f1e66ec0664c72
ryu <jeungwon28@gmail.com>
2024-03-12 14:43:04 +0900

    ADD: 동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용

--- Querydsl/querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ Querydsl/querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -29,6 +29,8 @@ class MemberJpaRepositoryTest {
     @Autowired
     MemberJpaRepository memberJpaRepository;
 
+    JPAQueryFactory queryFactory;
+
 
     @Test
     public void basicTest() {
@@ -67,9 +69,10 @@ class MemberJpaRepositoryTest {
         condition.setAgeLoe(40);
         condition.setTeamName("teamB");
 
-        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
 
         assertThat(result).extracting("username").containsExactly("member4");
     }
+
 
 }

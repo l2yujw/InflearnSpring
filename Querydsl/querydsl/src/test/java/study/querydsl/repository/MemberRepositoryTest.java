15830c24ad2f2a9418a666d90af9691cb09506d7
ryu <jeungwon28@gmail.com>
2024-03-12 15:01:02 +0900

    ADD: 스프링 데이터 JPA 리포지토리로 변경

---
+++ Querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,43 @@
+package study.querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    JPAQueryFactory queryFactory;
+
+
+    @Test
+    public void basicTest() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        Collection<Member> result1 = memberRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        Collection<Member> result2 = memberRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+    }
+}
